c6992f2ae4dec583f068752ead171e918b2a3236
edfherae <163173239+edfherae@users.noreply.github.com>
2026-07-08 17:05:59 +0300

    added basic page structure and data storage

---
+++ script.ts
@@ -0,0 +1,38 @@
+interface numbersDictionary {
+    [letter: string]: phoneNumber[]
+}
+
+interface phoneNumber {
+    name: string;
+    vacancy: string;
+    number: string
+}
+
+let numbersDictionary: numbersDictionary = {
+    "A": [],
+    "B": [],
+    "C": [],
+    "D": [],
+    "E": [],
+    "F": [],
+    "G": [],
+    "H": [],
+    "I": [],
+    "J": [],
+    "K": [],
+    "L": [],
+    "M": [],
+    "N": [],
+    "O": [],
+    "P": [],
+    "Q": [],
+    "R": [],
+    "S": [],
+    "T": [],
+    "U": [],
+    "V": [],
+    "W": [],
+    "X": [],
+    "Y": [],
+    "Z": []
+};
