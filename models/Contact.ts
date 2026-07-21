93942881a9852c256b23f781153edeb0b0372ad9
edfherae <163173239+edfherae@users.noreply.github.com>
2026-07-21 12:48:43 +0300

    Revert "Develop"

--- models/Contact.ts
+++
@@ -1,6 +0,0 @@
-export default interface Contact {
-    id: number;
-    name: string;
-    vacancy: string;
-    phoneNumber: string
-}
--- script.ts
+++ script.ts
@@ -1,85 +1,59 @@
-import Contact from "./models/Contact.js";
+interface numbersDictionary1 {
+    [letter: string]: phoneNumber[]
+}
 
-import { createContactStore } from "./services/createContactStore.js";
-import { Alphabet } from "./components/Alphabet.js";
-import { ContactList } from "./components/ContactList.js";
-import { AddNumberForm } from "./components/AddNumberForm.js";
-import { SearchModal } from "./components/searchModal.js";
-import { ChangeModal } from "./components/changeModal.js";
+interface phoneNumber {
+    name: string;
+    vacancy: string;
+    number: string
+}
 
-const localStorageData = localStorage.getItem("data");
-const contactStore = createContactStore(localStorageData ? JSON.parse(localStorageData) : []);
+const numbersDictionary: numbersDictionary1 = {
+    "A": [],
+    "B": [],
+    "C": [],
+    "D": [],
+    "E": [],
+    "F": [],
+    "G": [],
+    "H": [],
+    "I": [],
+    "J": [],
+    "K": [],
+    "L": [],
+    "M": [],
+    "N": [],
+    "O": [],
+    "P": [],
+    "Q": [],
+    "R": [],
+    "S": [],
+    "T": [],
+    "U": [],
+    "V": [],
+    "W": [],
+    "X": [],
+    "Y": [],
+    "Z": []
+};
 
-const addNumberForm = new AddNumberForm(
-    document.querySelector(".add-number-form") as HTMLFormElement, 
-    document.querySelector(".name-input") as HTMLInputElement, 
-    document.querySelector(".vacancy-input") as HTMLInputElement, 
-    document.querySelector(".number-input") as HTMLInputElement,
-    document.querySelector(".add-number-button") as HTMLButtonElement, 
-    onFormSubmit
-);
-const clearListButton = document.getElementById("clearListButton") as HTMLButtonElement;
-clearListButton.addEventListener("click", () => {
-    contactStore.clear();
-    contactList.clear();
-    alphabet.render(contactStore.getLettersCount(), onLetterSelect);
+const alphabet : Element | null = document.querySelector(".alphabet");
+
+if(alphabet) {
+    Object.entries(numbersDictionary).forEach(([key]) => {
+        const letter : HTMLDivElement = document.createElement("div");
+        letter.classList.add("letter");
+        letter.textContent = key;
+        // letter.addEventListener("click", () => alert(key))
+        alphabet.append(letter);
+    })
+} else console.log("alphabet is null");
+
+const nameInput = document.getElementById("nameInput") as HTMLInputElement;
+const vacancyInput = document.getElementById("vacancyInput") as HTMLInputElement;
+const numberInput = document.getElementById("numberInput") as HTMLInputElement;
+const addNumberButton = document.getElementById("addNumberButton") as HTMLInputElement;
+
+addNumberButton?.addEventListener("click", () => {
+    alert([nameInput.value, vacancyInput.value, numberInput.value]);
 });
-const searchButton = document.getElementById("searchNumberButton");
-searchButton?.addEventListener("click", () => searchModal.open());
-
-const alphabet = new Alphabet(document.querySelector(".alphabet") as HTMLDivElement);
-const contactList = new ContactList(document.querySelector(".numbers-output-container") as HTMLDivElement, openChangeModal, deleteContact, (letter) => contactStore.getContactsByLetter(letter));
-
-const searchModal = new SearchModal(
-    document.querySelector(".modal-window") as HTMLDialogElement,
-    document.querySelector(".modal-action") as HTMLHeadingElement, 
-    document.querySelector(".modal-name") as HTMLInputElement, 
-    document.querySelector(".modal-vacancy") as HTMLInputElement, 
-    document.querySelector(".modal-number") as HTMLInputElement, 
-    document.querySelector(".modal-submit") as HTMLButtonElement,
-    document.querySelector(".modal-close") as HTMLParagraphElement,
-    document.querySelector(".modal-output") as HTMLDivElement,
-    contactStore.searchContacts,
-    openChangeModal,
-    deleteContact
-);
-
-const changeModal = new ChangeModal(changeContact);
-function changeContact(contact : Contact) {
-    contactStore.changeContact(contact);
-    // if is active
-    //contactList.render(contactList.currentLetter, contactStore.getContactsByLetter(letter));
-    alphabet.render(contactStore.getLettersCount(), onLetterSelect);
-}
-
-// поиск контакта по айди от searchModal и передача его в changeModal
-function openChangeModal(id: number, rerenderComponent: () => void) {
-    const contact = contactStore.getContactById(id);
-    if(contact) changeModal.open(contact, () => rerenderComponent());
-    else alert("не найден такой контакт в базе")
-}
-
-function deleteContact(id: number) {
-    contactStore.deleteContact(id);
-    // if is active
-    //contactList.render(contactList.currentLetter, contactStore.getContactsByLetter(letter));
-    alphabet.render(contactStore.getLettersCount(), onLetterSelect);
-}
-
-function onLetterSelect(letter : string) {
-    contactList.render(letter, contactStore.getContactsByLetter(letter));
-}
-
-function onFormSubmit(contact : Contact) {
-    contactStore.addContact(contact);
-    alphabet.render(contactStore.getLettersCount(), onLetterSelect);
-
-    const firstLetter = contact.name[0].toUpperCase();
-    if(firstLetter === contactList.currentLetter) contactList.render(firstLetter, contactStore.getContactsByLetter(firstLetter))
-}
-
-alphabet.render(contactStore.getLettersCount(), onLetterSelect);
-
-window.addEventListener("pagehide", () => {
-    localStorage.setItem("data", JSON.stringify(contactStore.getAllContacts()));
-})
